c41e8868b8baafaa2d5d6ee6a99786939dc67528
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-11 15:46:12 -0400

    implemented arrow draw shoot and release

---
+++ Archery-Game/Assets/Scripts/ShootArrows.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArrows : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject arrowPrefab;
+
+    [SerializeField]
+    private Transform arrowTransform;
+
+    [SerializeField]
+    private float maximumArrowForce = 200f;
+    private float currentArrowForce;
+    
+    private float maxLerpTime = 1f;
+    private float currentLerpTime;
+
+    private Animator bowAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bowAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DrawBow();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            PowerUpBow();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ReleaseBow();
+        }
+    }
+
+    private void DrawBow()
+    {
+        bowAnimator.SetBool("drawing", true);
+    }
+
+    private void PowerUpBow()
+    {
+        currentLerpTime += Time.deltaTime;
+
+        if (currentLerpTime > maxLerpTime)
+        {
+            currentLerpTime = maxLerpTime;
+        }
+
+        float perc = currentLerpTime / maxLerpTime;
+        currentArrowForce = Mathf.Lerp(0, maximumArrowForce, perc);
+    }
+
+    private void ReleaseBow()
+    {
+        bowAnimator.SetBool("drawing", false);
+
+        GameObject arrow = Instantiate(arrowPrefab, arrowTransform.position, arrowTransform.rotation) as GameObject;
+        arrow.GetComponent<Rigidbody>().AddForce(arrowTransform.forward * currentArrowForce);
+
+        currentLerpTime = 0;
+        currentArrowForce = 0;
+    }
+}
