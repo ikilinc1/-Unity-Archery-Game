1bb0792fa9e314fa361a124b40faac556fc2c89f
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-11 14:38:24 -0400

    started with bow controller

---
+++ Archery-Game/Assets/Scripts/BowController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowController : MonoBehaviour
+{
+    private float rotationSpeed;
+    private Vector2 rotationRange;
+    private Vector3 targetAngle;
+    private Quaternion startRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
