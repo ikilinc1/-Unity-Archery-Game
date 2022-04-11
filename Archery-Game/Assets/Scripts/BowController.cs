01d7946dd57dcdc1fb4c6a2743128179b01fdfd5
ikilinc1 <ikilinc1@binghamton.edu>
2022-04-11 15:15:06 -0400

    finished basic mouse movement for the bow

--- Archery-Game/Assets/Scripts/BowController.cs
+++ Archery-Game/Assets/Scripts/BowController.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class BowController : MonoBehaviour
 {
-    private float rotationSpeed;
-    private Vector2 rotationRange;
+    [SerializeField]
+    private float rotationSpeed = 0.5f;
+    
+    [SerializeField]
+    private Vector2 rotationRange = new Vector2(50, 95);
+    
     private Vector3 targetAngle;
     private Quaternion startRotation;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        startRotation = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        targetAngle.y += Input.GetAxis("Mouse X") * rotationSpeed;
+        targetAngle.x += Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        targetAngle.y = Mathf.Clamp(targetAngle.y, -rotationRange.y * 0.5f, rotationRange.y * 0.5f);
+        targetAngle.x = Mathf.Clamp(targetAngle.x, -rotationRange.x * 0.5f, rotationRange.x * 0.5f);
         
+        Quaternion targetRotation = Quaternion.Euler(-targetAngle.x, targetAngle.y, 0);
+        transform.localRotation = startRotation * targetRotation;
     }
 }
